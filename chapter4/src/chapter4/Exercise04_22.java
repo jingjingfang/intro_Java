c0ae073723303a4d40e7786dbbac1faefd19daef
jjfca <jjfca@LAPTOP-3H2PFK34>
2020-02-22 20:31:34 -0500

    initial commit

---
+++ chapter4/src/chapter4/Exercise04_22.java
@@ -0,0 +1,28 @@
+/*Write a program that prompts the user to enter two strings and reports 
+ * whether the second string is a substring of the first string.
+ */
+
+package chapter4;
+
+
+import java.util.Scanner;
+
+public class Exercise04_22 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+	    System.out.print("Enter string s1: ");
+	    String s1 = input.nextLine();
+	    
+	    System.out.print("Enter string s2: ");
+	    String s2 = input.nextLine();
+	    
+	    if (s1.indexOf(s2) != -1) {
+	      System.out.println(s2 + " is a substring of " + s1);
+	    }
+	    else {
+	      System.out.println(s2 + " is not a substring of " + s1);
+	    }
+	}
+
+}
